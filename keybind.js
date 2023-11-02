f355bd47741ede0ef84c2dd8eac88da7b69e44c0
Moanrisy <moanrisy@gmail.com>
2023-11-02 22:20:05 +0700

    fix: c-l keybind catch c-s-l too

--- keybind.js
+++ keybind.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 
   function doc_keyUp(e) {
-    if (e.ctrlKey && e.code === 'KeyL') {
+    if (e.shiftKey && e.ctrlKey && e.code === 'KeyL') {
+      // if not handled, if block using c-l will catch c-s-l too
+    } else if (e.ctrlKey && e.code === 'KeyL') {
       // prevent c-l firefox builtin shortcut
       e.preventDefault();
       $("#dialog").dialog();
